b1f4f24a23b43ce5a4f67291cd05d49edaf509ec
mursalinmirme <mursalinmir02@gmail.com>
2023-12-22 12:15:26 +0600

    I have make the add task and my-todo routes for added and view tasks

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
+require('dotenv').config()
 app.use(cors())
 app.use(express.json())
 app.get('', (req, res)  => {
     res.send("The spark tasker is running now...")
 })
 
-
+console.log(process.env.DB_NAME);
+console.log(process.env.DB_PASSWORD);
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://<spark-tasker>:<lhhjAmKjOPqmF2aY>@mursalin.bxh3q56.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASSWORD}@mursalin.bxh3q56.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -27,7 +30,22 @@ async function run() {
   try {
     const taskCollections = client.db('spark-tasker').collection('my-tasks');
 
+    app.post('/add-tasks', async(req, res) =>{
+        const receive = req.body;
+        console.log(receive);
+        const result = await taskCollections.insertOne(receive)
+        res.send(result)
+        console.log(result);
+    })
+    app.get('/my-todos', async(req, res) => {
+        const email = req.query.email;
+        console.log(email);
+        const result = await taskCollections.find({taskUser: email}).toArray();
+        console.log('resp', result);
+        res.send(result)
+    })
 
+    
 
 
     client.db("admin").command({ ping: 1 });
